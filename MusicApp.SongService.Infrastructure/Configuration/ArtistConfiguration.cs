53f7fad3306ef416a5e18f97cceb9babf2cd71fb
rediska235 <vanya.bobr.12@gmail.com>
2023-07-17 17:37:59 +0300

    Add entities configuration

---
+++ MusicApp.SongService.Infrastructure/Configuration/ArtistConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using MusicApp.SongService.Domain.Entities;
+
+namespace MusicApp.SongService.Infrastructure.Configuration;
+
+public class ArtistConfiguration : IEntityTypeConfiguration<Artist>
+{
+    public void Configure(EntityTypeBuilder<Artist> builder)
+    {
+        builder.HasKey(s => s.Id);
+        builder.Property(s => s.Username).IsRequired().HasMaxLength(32);
+    }
+}
