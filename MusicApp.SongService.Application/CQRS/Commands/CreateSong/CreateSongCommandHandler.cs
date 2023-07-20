b1f6318dd38eeb6ddb95ebe35c3a813b4b4b37ea
rediska235 <vanya.bobr.12@gmail.com>
2023-07-20 18:37:15 +0300

    Fix AutoMapper error

--- MusicApp.SongService.Application/CQRS/Commands/CreateSong/CreateSongCommandHandler.cs
+++ MusicApp.SongService.Application/CQRS/Commands/CreateSong/CreateSongCommandHandler.cs
@@ -1,11 +1,12 @@
 ﻿using AutoMapper;
 using MediatR;
+using MusicApp.SongService.Application.DTOs;
 using MusicApp.SongService.Application.Repositories;
 using MusicApp.SongService.Domain.Entities;
 
 namespace MusicApp.SongService.Application.CQRS.Commands.CreateSong;
 
-public class CreateSongCommandHandler : IRequestHandler<CreateSongCommand, Song>
+public class CreateSongCommandHandler : IRequestHandler<CreateSongCommand, SongOutputDto>
 {
     private readonly ISongRepository _repository;
     private readonly IMapper _mapper;
@@ -16,13 +17,13 @@ public class CreateSongCommandHandler : IRequestHandler<CreateSongCommand, Song>
         _mapper = mapper;
     }
 
-    public async Task<Song> Handle(CreateSongCommand request, CancellationToken cancellationToken)
+    public async Task<SongOutputDto> Handle(CreateSongCommand request, CancellationToken cancellationToken)
     {
         var song = _mapper.Map<Song>(request);
 
         await _repository.CreateAsync(song, cancellationToken);
         await _repository.SaveChangesAsync(cancellationToken);
 
-        return song;
+        return _mapper.Map<SongOutputDto>(song);
     }
 }
